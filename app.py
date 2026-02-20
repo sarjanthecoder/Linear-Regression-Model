158f75d284820fcd98709696809a27964df0f7e3
sarjan <codecrafter2005@gmail.com>
2026-02-20 08:45:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def predict():
         return jsonify({"error": f"Prediction failed: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=False, host='127.0.0.1', port=5000)
